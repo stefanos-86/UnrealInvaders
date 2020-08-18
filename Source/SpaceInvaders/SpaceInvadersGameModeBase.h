1096ea0c3860721f6ddfd2a8c263e7007137e9d8
Stefano <stefanos_86@libero.it>
2020-08-18 19:19:30 +0200

    Initial commit before UE can wreak everything.

---
+++ Source/SpaceInvaders/SpaceInvadersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceInvadersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEINVADERS_API ASpaceInvadersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
